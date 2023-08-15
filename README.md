5e95e7740e9ef50f1e681a6a8adae75a5ad81ef9
Moeed-Elahi <60301564+Moeed-Elahi@users.noreply.github.com>
2023-08-15 19:53:04 +0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # WeatherApp
+# Link to vercel https://weather-app-tan-pi.vercel.app/
+
+# Weather App
+
+This is a simple weather application built using React, Next.js, and the OpenWeatherMap API. The app allows users to search for the current weather by city and displays the weather conditions, temperature, and an icon representing the weather.
+
+## Table of Contents
+
+- [Description](#description)
+- [Requirements](#requirements)
+- [Development](#development)
+- [Deployment](#deployment)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Description
+
+This project is a part of my application for [Company Name]. The objective was to build a weather application using React, Next.js, and the OpenWeatherMap API. The app fetches current weather data for a given city and displays it in a user-friendly interface.
+
+## Requirements
+
+- React
+- Next.js
+- Redux (or alternative state management solution)
+- Material-UI (for UI components)
+- Axios (for making API requests)
+- [OpenWeatherMap API Key](https://openweathermap.org/api) (sign up for a free API key)
+- Node.js and npm
+
+## Development
+
+To set up the project for development:
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory: `cd weather-app`
+3. Install dependencies: `npm install`
+
+### Running the Development Server
+
+To start the development server:
+
+```bash
+npm run dev
--- src/Views/Pages/Main/S1_Header/index.jsx
+++ src/Views/Pages/Main/S1_Header/index.jsx
@@ -4,11 +4,10 @@ import { TextField, Button, Typography } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { setWeather } from '../../../../Redux/store/weatherSlice';
 import { styled } from '@mui/system';
-import { Grid ,Container } from "@mui/material"
+import { Grid, Container } from "@mui/material"
 import { SectionHeading, SectionSubHeading } from '@/Views/Styles/style';
-const API_KEY = '1aae245da000437dbba122742231508'; // Replace with your API key
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY; // Replace with your API key
 const API_URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
-
 const StyledWeatherApp = styled('div')`
   display: flex;
   justify-content: center;
@@ -20,7 +19,6 @@ const WeatherApp = () => {
   const weatherData = useSelector((state) => state.weather);
   const [city, setCity] = useState('');
   const [error, setError] = useState(null);
-console.log("weatherData",weatherData);
   const fetchWeatherData = async () => {
     try {
       setError(null);
@@ -32,51 +30,50 @@ console.log("weatherData",weatherData);
   };
   return (
     <>
-        <SectionHeading>Weather Update</SectionHeading>
-        <StyledWeatherApp>
-    <Container >
-      
-      <Grid container>
-        <Grid xs={12} md={6}>
-        <SectionHeading fs="20px" align="left" p="20px 0">Enter City Name</SectionHeading>
-      <TextField
-        label="name"
-        variant="outlined"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        sx={{ width: 300, marginTop: 0 }}
-      />
-      <br/>
-      <Button variant="contained" onClick={fetchWeatherData} sx={{ marginTop: 5 }}>
-        Search
-      </Button>
+      <SectionHeading>Weather Update</SectionHeading>
+      <StyledWeatherApp>
+        <Container >
+          <Grid container>
+            <Grid item xs={12} md={6}>
+              <SectionHeading fs="20px" align="left" p="20px 0">Enter City Name</SectionHeading>
+              <TextField
+                label="name"
+                variant="outlined"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                sx={{ width: 300, marginTop: 0 }}
+              />
+              <br />
+              <Button variant="contained" onClick={fetchWeatherData} sx={{ marginTop: 5 }}>
+                Search
+              </Button>
 
-      {error && <Typography color="error" sx={{ marginTop: 5 }}>{error}</Typography>}
+              {error && <Typography color="error" sx={{ marginTop: 5 }}>{error}</Typography>}
 
-        </Grid>
-        <Grid xs={12} md={6}>
-        {weatherData && (
-        <div>
-          <SectionSubHeading variant="h5" sx={{ marginTop: 20 }}>
-            {weatherData.location.name}
-          </SectionSubHeading>
-          <Typography >
-          Local Time:  {weatherData.location.localtime}
-          </Typography>
-          <Typography>Temperature: {weatherData.current.temp_c}°C</Typography>
-          <Typography>Condition: {weatherData.current.condition.text}</Typography>
-          <Typography>humidity: {weatherData.current.humidity}</Typography>
-          <Typography>feelslike: {weatherData.current.feelslike_c}°C</Typography>
-          <img src={weatherData.current.condition.icon} alt="Weather Icon" />
-        </div>
-      )}
-        </Grid>
-      </Grid>
-     
-   
-    
-    </Container>
-    </StyledWeatherApp>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              {weatherData && (
+                <div>
+                  <SectionSubHeading variant="h5" sx={{ marginTop: 20 }}>
+                    {weatherData.location.name}
+                  </SectionSubHeading>
+                  <Typography >
+                    Local Time:  {weatherData.location.localtime}
+                  </Typography>
+                  <Typography>Temperature: {weatherData.current.temp_c}°C</Typography>
+                  <Typography>Condition: {weatherData.current.condition.text}</Typography>
+                  <Typography>humidity: {weatherData.current.humidity}</Typography>
+                  <Typography>feelslike: {weatherData.current.feelslike_c}°C</Typography>
+                  <img src={weatherData.current.condition.icon} alt="Weather Icon" />
+                </div>
+              )}
+            </Grid>
+          </Grid>
+
+
+
+        </Container>
+      </StyledWeatherApp>
     </>
   );
 };
